267600288b5bd0bab92848fc3abbdb0c32e209dc
Higor Matheus Paes <123217504+higormatheuspaes@users.noreply.github.com>
2023-05-13 18:38:02 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 100)
+})
+
+let menu = document.querySelector('#menu-icon')
+let navList = document.querySelector('.navlist')
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navList.classList.toggle('open')
+}
